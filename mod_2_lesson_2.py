623152a9dc6746f46b62eb70010c199ccaaae112
Kristine-M <kma28@uic.edu>
2024-02-15 22:10:39 -0600

    on task 2

--- mod_2_lesson_2.py
+++ mod_2_lesson_2.py
@@ -7,3 +7,13 @@ elif number == 0:
     print("The number is zero.")
 else :
     print("The number is negative.")
+
+#2.1
+choice = input("Do you choose the path to the left or right? ")
+
+if choice == "left":
+    print("You find a treasure chest!")
+elif choice == "right":
+    print("You encounter a dragon!")
+else:
+    print("Invalid choice!")
